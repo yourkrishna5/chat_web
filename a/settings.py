afdb656a0d67394882e5ecd7845ffe821eaf3cf5
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-15 18:46:35 +0545

    Update settings.py

--- a/settings.py
+++ a/settings.py
@@ -39,13 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "new.apps.NewConfig",
-    "rest_framework"
+    "rest_framework",
+"corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+"corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
