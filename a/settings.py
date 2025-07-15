285c9653fd1decdc5b4b45cdb220b88fcc87dc11
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-15 08:37:05 +0545

    Update settings.py

--- a/settings.py
+++ a/settings.py
@@ -128,3 +128,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage" 
